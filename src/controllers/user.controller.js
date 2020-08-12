c31d950c5b9eb9133f0acada0f5975df47c11cd0
Naggas07 <rober.araya.gil@gmail.com>
2020-08-12 18:55:14 +0200

    user login controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,3 +18,28 @@ module.exports.create = (req, res, next) => {
     })
     .catch(next);
 };
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "email and password required" });
+  }
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({ message: "User not found" });
+      } else {
+        return user.comparePassword(password).then((match) => {
+          if (match) {
+            req.session.user = user;
+            res.status(200).json(user);
+          } else {
+            res.status(404).json({ message: "User not found" });
+          }
+        });
+      }
+    })
+    .catch(next);
+};
