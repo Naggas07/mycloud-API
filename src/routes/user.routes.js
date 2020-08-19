98991ac037427b2830bc98ae73699c7009cecec7
Naggas07 <rober.araya.gil@gmail.com>
2020-08-19 16:22:24 +0200

    user register fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
+const rolMiddleware = require("../middlewares/rol.middlewares");
 
-router.post("/register", userController.create);
-router.post("/login", userController.login);
-router.post("/logout", userController.logout);
-router.post("/update/:id", userController.updateUser);
+router.post(
+  "/register",
+  authMiddleware.isAuthenticated,
+  rolMiddleware.isAdmin,
+  userController.create
+);
+router.post("/login", authMiddleware.isNotAuthenticated, userController.login);
+router.post("/logout", authMiddleware.isAuthenticated, userController.logout);
+router.post(
+  "/update/:id",
+  authMiddleware.isAuthenticated,
+  userController.updateUser
+);
 
 module.exports = router;
