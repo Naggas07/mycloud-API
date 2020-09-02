46c2f6a0d6ce512b20c94935f346a78077b172b8
Naggas07 <rober.araya.gil@gmail.com>
2020-09-02 09:41:07 +0200

    fixed deprecation warning of mongoose create index

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/myCloud";
 
+mongoose.set("useCreateIndex", true);
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
